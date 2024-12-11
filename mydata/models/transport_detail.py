c80f7bf56da9cac3aea75f6ec8b6ebb3284f9894
Thanos Theodoridis <at@atworks.gr>
2024-12-11 11:54:05 +0200

    Add docstrings, validation

--- mydata/models/transport_detail.py
+++ mydata/models/transport_detail.py
@@ -1,6 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class TransportDetail:
-    vehicleNumber: str = ""
+    """
+    Represents transport details for a transaction.
+
+    Attributes:
+        vehicleNumber (str): Required. Transport means number. Maximum
+        allowed length is 50.
+    """
+
+    vehicleNumber: str = field(default="", metadata={"max_length": 50})
+
+    def __post_init__(self):
+        if len(self.vehicleNumber) > 50:
+            raise ValueError("vehicleNumber exceeds maximum length of 50 characters.")
