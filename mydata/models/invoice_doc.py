40b9204aa68f7784cd9146d56cbbbb50abaaf94c
Thanos Theodoridis <at@atworks.gr>
2024-12-12 10:32:02 +0200

    Get rid of those for now

--- mydata/models/invoice_doc.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from typing import List
-
-from .invoice import Invoice
-
-
-@dataclass
-class InvoicesDoc:
-    invoice: List[Invoice] = field(default_factory=list)
